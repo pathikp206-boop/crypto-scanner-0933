f1a59a02e6807c3e4f9c932aae7667bcf53ff417
pathikp206-boop <pathikp206@gmail.com>
2026-07-04 20:34:03 +0000

    binance

--- binance_api.py
+++ binance_api.py
@@ -1,5 +1,4 @@
 import requests
-
 from config import BINANCE_FUTURES
 
 
@@ -7,12 +6,19 @@ def get_symbols():
 
     url = f"{BINANCE_FUTURES}/fapi/v1/exchangeInfo"
 
-    data = requests.get(url).json()
+    response = requests.get(url, timeout=30)
+
+    print("Status:", response.status_code)
+    print("Response:", response.text[:1000])
+
+    data = response.json()
+
+    if "symbols" not in data:
+        raise Exception(f"Unexpected Binance response: {data}")
 
     symbols = []
 
     for s in data["symbols"]:
-
         if (
             s["contractType"] == "PERPETUAL"
             and s["quoteAsset"] == "USDT"
