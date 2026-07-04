a84be18712bc2d2d117b3b9962d66b611d796c70
pathikp206-boop <pathikp206@gmail.com>
2026-07-04 20:16:22 +0000

    Initial Telegram Bot

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import os
 import requests
+from datetime import datetime
 
+# Environment Variables
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
 
-message = "🚀 Crypto Scanner Started new one Successfully!"
+# Current Time
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+# Telegram Message
+message = f"""
+🚀 Crypto Scanner Online
+
+🕒 Time: {current_time}
+
+✅ Status: Running
+
+🤖 Hosted on Railway
+"""
+
+# Telegram API URL
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
+# Send Message
+response = requests.post(
     url,
     data={
         "chat_id": CHANNEL_ID,
@@ -16,4 +32,8 @@ requests.post(
     }
 )
 
-print("Telegram message sent successfully.")
+# Check Result
+if response.status_code == 200:
+    print("✅ Telegram message sent successfully.")
+else:
+    print(f"❌ Failed to send message: {response.text}")
